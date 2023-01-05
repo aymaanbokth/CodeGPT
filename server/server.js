40c946292a4d7ec97b9c3822dc048115d5a73558
Aymaan Bokth <aabokth@gmail.com>
2023-01-04 22:33:03 -0500

    update server URL

--- server/server.js
+++ server/server.js
@@ -44,6 +44,6 @@ app.post('/', async (req, res) => {
         
 })
 
-app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
+app.listen(5000, () => console.log('Server is running on port https://codegpt-eoq1.onrender.com'));
 
     
