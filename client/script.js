c49bc972b9f917bb271536bea43fc6eec0672db4
Aymaan Bokth <aabokth@gmail.com>
2023-01-04 21:49:34 -0500

    first-commit

---
+++ client/script.js
@@ -0,0 +1,122 @@
+// Import images for user and AI bot respectively
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+// Function that creates the "..." animated loading icon for when the 
+// bot is processing the request.
+function loader(element) {
+  element.textContent = ''
+  
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  }, 300);
+}
+
+// Function to have a typewriter effect for when the bot's response is being ouput
+function typeText(element, text) {
+  let index = 0;
+
+  // Iterates through each letter of the text and change's the element's inner html to 
+  // letter by letter to give a "typing effect"
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    }
+    else {
+      clearInterval(interval);
+    }
+  }, 20)
+}
+
+// generate unique ID for each message div of bot
+// necessary for typing text effect for that specific reply
+// without unique ID, typing text will work on every element
+function generateUniqueID() {
+  const timeStamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timeStamp}-${hexadecimalString}`;
+}
+
+// Function that creates alternating stripes for the user's input and the bot's output
+function chatStripe (isAi, value, uniqueID) {
+  return (
+    `
+      <div class="wrapper ${isAi && 'ai'}">
+        <div class="chat">
+          <div class="profile">
+            <img src="${isAi ? bot : user}" alt="${isAi ? 'bot' : 'user'}"/>
+          </div>
+          <div class="message" id=${uniqueID}>${value}</div>
+        </div>
+      </div>
+    `
+  )
+}
+
+// Handles cases when the user hits submit
+const handleSubmit = async (e) => {
+  e.preventDefault();
+
+  const data = new FormData(form);
+
+  // user's chat stripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+
+  form.reset();
+
+  // bot's chat stripe
+  const uniqueID = generateUniqueID();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueID);
+
+  // scroll functionality
+  chatContainer.scrollTop = chatContainer.scrollHeight;
+
+  const messageDiv = document.getElementById(uniqueID);
+  loader(messageDiv);
+
+  const response = await fetch('http://localhost:5000', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      prompt: data.get('prompt')
+    })
+  })
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = '';
+
+  // If response is valid, call typeText() to output reponse.
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+    typeText(messageDiv, parsedData);
+  }
+  // If invalid, output error message.
+  else {
+    const err = await response.text();
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
+}
+
+
+form.addEventListener('submit', handleSubmit);
+form.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+})
